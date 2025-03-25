96f3e2c66011fe7772e9dc8749220dde67daef60
Gabriel Kereklidis Paulin <gabriel.paulin111@gmail.com>
2025-03-25 10:24:39 +0100

    database connection & character model

---
+++ src/models/Character.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const CharacterSchema = new mongoose.Schema({
+    name: { type: String, required: true }, 
+    weight_class: { type: String, enum: ["light", "medium", "heavy"], required: true },
+    movement_speed: { type: Number, required: true }, 
+    original_game_series: { type: String, required: true },
+    icon: { type: String },
+    image: { type: String },
+    model: { type: String },
+    tier_ranking: { type: String, enum: ["S", "A", "B", "C", "D"], required: true },
+    notable_players: { type: [string], default: [] }
+});
+
+const Character = mongoose.model("Character", CharacterSchema); 
+module.exports = Character; 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
+require("dotenv").config();
 const express = require("express"); 
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express(); 
 
 app.use(cors());
 app.use(express.json()); 
 
 const PORT = process.env.PORT || 4000; // ändra port? 
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB!"))
+.catcg(err => console.error("MongoDB connection error:", err)); 
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
