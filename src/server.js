f03b62e103683d2e7d27bf17fade95a64c423da7
Gabriel Kereklidis Paulin <gabriel.paulin111@gmail.com>
2025-03-27 12:05:25 +0100

    save

--- src/server.js
+++ src/server.js
@@ -7,26 +7,26 @@ const characterRoutes = require("./routes/characterRoutes");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB!"))
   .catch(err => {
     console.error("MongoDB connection error:", err);
-    process.exit(1); // Stänger ner servern om den inte kan ansluta
+    process.exit(1); 
   });
 
-// Lyssna på anslutningsfel
+
 mongoose.connection.on("error", (err) => {
   console.error("Mongoose connection error:", err);
 });
-// Routes
+
 app.use("/characters", characterRoutes);
 
 app.get("/", (req, res) => res.send("API is running..."));
 
-// Start Server
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
